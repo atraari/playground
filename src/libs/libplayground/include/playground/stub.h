8eb85e5c349d08de2759dc3cea348a116f16e0b6
atraari@gmail.com <atraari@gmail.com>
2016-05-02 03:49:03 +0200

    Add stub function to libplayground.

---
+++ src/libs/libplayground/include/playground/stub.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2016, atraari
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE" for details.
+ *
+ */
+
+#ifndef _PLAYGROUND_STUB_H_
+#define _PLAYGROUND_STUB_H_
+
+void playground_stub();
+
+#endif /* _PLAYGROUND_STUB_H_ */
---
+++ src/libs/libplayground/src/stub.c
@@ -0,0 +1,14 @@
+/*
+ * Copyright 2016, atraari
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE" for details.
+ *
+ */
+
+void
+playground_stub()
+{
+	// This is a stub.
+}
