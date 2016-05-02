2db99059e93af68402a9acd1202cae0e6201e1e6
atraari@gmail.com <atraari@gmail.com>
2016-05-02 02:29:21 +0200

    Added basic files needed for a simple build

---
+++ src/apps/init/src/init.c
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2016, atraari
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE" for details.
+ *
+ */
+
+#include <stdio.h>
+#include <autoconf.h>
+
+int main(void)
+{
+    printf("Init started and exiting...\n");
+    return 0;
+}
