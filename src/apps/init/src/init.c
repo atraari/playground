3cbc874826a5b6b36297104a1217470235e8a3c7
atraari@gmail.com <atraari@gmail.com>
2016-05-04 19:38:26 +0200

    Remove undeclared functions

--- src/apps/init/src/init.c
+++ src/apps/init/src/init.c
@@ -179,8 +179,6 @@ int main(void)
     printf("Switching to a safer, bigger stack\n");
     void *res;
     int error = sel4utils_run_on_stack(&env.vspace, main_continued, NULL, &res);
-    test_assert_fatal(error == 0);
-    test_assert_fatal(res == 0);
 
     return 0;
 }
